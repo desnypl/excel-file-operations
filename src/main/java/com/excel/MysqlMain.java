ff5574a5120fe1e418b1e34f7abc495a89ee8a95
Anish Antony <Anish Antony>
2017-05-31 09:04:48 +0530

    db connections

---
+++ src/main/java/com/excel/MysqlMain.java
@@ -0,0 +1,24 @@
+package com.excel;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+
+public class MysqlMain {
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+		Class.forName("com.mysql.jdbc.Driver");
+		Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3307/excel-operations","root","root");
+		Statement stmt = con.createStatement();
+		ResultSet rs = stmt.executeQuery("select * from mytable");
+		while(rs.next()){
+			System.out.println(rs.getInt(1));
+			System.out.println(rs.getString(2));
+			System.out.println(rs.getInt(3));
+		}
+		
+	}
+
+}
